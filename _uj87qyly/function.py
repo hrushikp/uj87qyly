9d45338b6e9de08c23126299fc54289965b6f9e5
hrushikp <59475008+hrushikp@users.noreply.github.com>
2021-11-14 23:03:39 +0100

    Delete _uj87qyly directory

--- _uj87qyly/function.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from ipywidgets import interact,fixed
-from PIL import Image
-def imshow (X,resize=None):
-  image=Image.fromarray(X)
-  if resize!=None:
-    image=image.resize(resize)
-  return image.show()
