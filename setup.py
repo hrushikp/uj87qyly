70593abd7ad845e5faae5ac337dc73e56a448b2d
hrushikp <59475008+hrushikp@users.noreply.github.com>
2021-11-14 23:13:18 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-from setuptools import find_packages
-setup(name='uj87qyly',
-      version='0.1',
-      author='DSSS',
-      author_email='hrushik.perumalla@fau.de',
-      packages=find_packages(),
-      install_requires=['numpy', 'Pillow', 'ipywidgets'])
