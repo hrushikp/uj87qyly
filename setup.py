6dfef9f9e60247e499c6d262d347626346c5b4f0
hrushik_perumalla <hrushikperumalla@gmail.com>
2021-11-15 08:20:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 from setuptools import find_packages
-setup(name='na69tyzy',
+setup(name='uj87qyly',
       version='0.1',
       author='DSSS',
-      author_email='shobha.rajanna@fau.de',
+      author_email='hrushik.perumalla@fau.de',
       packages=find_packages(),
       install_requires=['numpy','Pillow','ipywidgets'])
       
